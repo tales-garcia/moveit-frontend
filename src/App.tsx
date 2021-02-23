3fad25d8c9691af7577c3c19330270f257b40fe4
Tales Garcia <garcia.tales@gmail.com>
2021-02-23 18:03:39 -0300

    feat(app): use theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
+import GlobalStyle from './styles/global';
+
+const theme = {
+  white: '#FFF',
+  background: '#F2F3F5',
+  grayLine: '#DCDDE0',
+  text: '#666666',
+  textHighlight: '#B3B9FF',
+  title: '#2E384D',
+  red: '#E83F5B',
+  green: '#4CD62B',
+  blue: '#5965E0',
+  darkBlue: '#4953B8',
+  twitterBlue: '#2AA9E0'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
   );
 }
 
