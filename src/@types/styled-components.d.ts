703d6b370a3e2a3d1c7283a224d9673dfd0c6bf6
Tales Garcia <garcia.tales@gmail.com>
2021-02-23 18:04:12 -0300

    chore: add types to default theme

---
+++ src/@types/styled-components.d.ts
@@ -0,0 +1,17 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    white: string,
+    background: string,
+    grayLine: string,
+    text: string,
+    textHighlight: string,
+    title: string,
+    red: string,
+    green: string,
+    blue: string,
+    darkBlue: string,
+    twitterBlue: string
+  }
+}
