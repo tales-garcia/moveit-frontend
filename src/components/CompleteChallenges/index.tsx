ab64d602cacec7172fac3bdc2084392bede32e7c
Tales Garcia <garcia.tales@gmail.com>
2021-02-24 19:16:36 -0300

    feat(components): create completed chalenges component

---
+++ src/components/CompleteChallenges/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CompleteChallenges: React.FC = () => {
+  return (
+    <Container>
+        <span>Desafios Completos</span>
+        <span>5</span>
+    </Container>
+  );
+}
+
+export default CompleteChallenges;
