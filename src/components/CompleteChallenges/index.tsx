6f3a35672f94de38c2d3b3b4d54587c1c2e13883
Tales Garcia <garcia.tales@gmail.com>
2021-02-25 19:51:00 -0300

    feat(completed challenges): display user's completed challenges count

--- src/components/CompleteChallenges/index.tsx
+++ src/components/CompleteChallenges/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useChallenge } from '../../hooks/challenge';
 
 import { Container } from './styles';
 
 const CompleteChallenges: React.FC = () => {
+  const { challengesCompleted } = useChallenge();
+
   return (
     <Container>
         <span>Desafios Completos</span>
-        <span>5</span>
+        <span>{challengesCompleted}</span>
     </Container>
   );
 }
