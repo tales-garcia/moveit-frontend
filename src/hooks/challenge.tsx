61ecd820582d88426d4f405a674befdd3605c2e1
Tales Garcia <garcia.tales@gmail.com>
2021-02-25 19:48:01 -0300

    feat(challenge hook): implement all features

--- src/hooks/challenge.tsx
+++ src/hooks/challenge.tsx
@@ -1,13 +1,26 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { createContext, useContext } from "react";
+import challenges from '../../challenges.json';
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 
 interface ChallengeContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
+    experienceToNextLevel: number;
+    hasFinishedCycle: boolean;
 
+    finishChallenge(experience: number): void;
     levelUp(): void;
     startNewChallenge(): void;
+    resetChallenge(): void;
+    toggleCycle(): void;
 }
 
 export const challengeContext = createContext({} as ChallengeContextData);
@@ -24,6 +37,9 @@ const ChallengeProvider: React.FC = ({ children }) => {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
+    const [activeChallenge, setActiveChallenge] = useState<Challenge | null>(null);
+    const [hasFinishedCycle, setHasFinishedCycle] = useState(false);
+    const experienceToNextLevel = useMemo(() => Math.pow((level + 1) * 4, 2), [level]);
 
     const levelUp = useCallback(
         () => {
@@ -34,9 +50,40 @@ const ChallengeProvider: React.FC = ({ children }) => {
 
     const startNewChallenge = useCallback(
         () => {
+            const randomIndex = Math.floor(Math.random() * challenges.length);
 
+            setActiveChallenge((challenges as Challenge[])[randomIndex]);
         },
         []
+    );
+
+    const resetChallenge = useCallback(
+        () => {
+            setActiveChallenge(null as Challenge);
+            setHasFinishedCycle(false);
+        },
+        []
+    );
+
+    const toggleCycle = useCallback(
+        () => {
+            setHasFinishedCycle(!hasFinishedCycle);
+        },
+        [hasFinishedCycle]
+    );
+
+    const finishChallenge = useCallback(
+        (experience) => {
+            setChallengesCompleted(challengesCompleted + 1);
+            if (currentExperience + experience < experienceToNextLevel) {
+                setCurrentExperience(currentExperience + experience);
+            } else {
+                const remainingExperience = (currentExperience + experience) - experienceToNextLevel;
+                setCurrentExperience(remainingExperience);
+                levelUp();
+            }
+        },
+        [challengesCompleted, currentExperience, experienceToNextLevel]
     );
 
     return (
@@ -44,8 +91,14 @@ const ChallengeProvider: React.FC = ({ children }) => {
             level,
             currentExperience,
             challengesCompleted,
+            experienceToNextLevel,
+            hasFinishedCycle,
             levelUp,
-            startNewChallenge
+            startNewChallenge,
+            resetChallenge,
+            finishChallenge,
+            toggleCycle,
+            activeChallenge
         }}>
             {children}
         </challengeContext.Provider>
